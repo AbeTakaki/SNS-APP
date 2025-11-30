93afb7c54d15fd7478d0d46c98e1e24d7f95ef76
AbeTakaki <abetakaki0101@gmail.com>
2025-11-30 17:04:53 +0900

    feat: フォロー中の投稿を表示するように修正

--- src/app/Http/Controllers/Xweet/IndexController.php
+++ src/app/Http/Controllers/Xweet/IndexController.php
@@ -8,21 +8,33 @@ use App\Models\Xweet;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Follows;
 
 class IndexController extends Controller
 {
     public function __invoke(Request $request) : View
     {
-        $xweets = Xweet::all();
-
         $loginId = Auth::id();
+
         if ($loginId) {
+            $users =  Follows::where('following_user_id', $loginId)->get();
+            $followUserIds = array(
+                0 => (int)$loginId,
+            );
+
+            foreach($users as $user) {
+                $followUserIds[] = $user->followed_user_id;
+            }
+
+            $xweets = Xweet::whereIn('user_id', $followUserIds)->get();
+
             $loginUser = User::where('id', $loginId)->first();
             return view('xweet.index')->with([
                 'userName' => $loginUser->user_name,
                 'xweets' => $xweets,
             ]);
         } else {
+            $xweets = Xweet::all();
             return view('xweet.index')->with([
                 'xweets' => $xweets,
             ]);
