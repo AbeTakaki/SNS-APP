d96d55b982ab28a5b918245f20d12172868e0106
AbeTakaki <abetakaki0101@gmail.com>
2025-11-30 10:28:20 +0900

    feat: フォロー・フォロワー確認機能の実装

--- src/app/Http/Controllers/User/FollowsController.php
+++ src/app/Http/Controllers/User/FollowsController.php
@@ -4,14 +4,28 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Follows;
+use Illuminate\View\View;
 
 class FollowsController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request, string $userName): string
+    public function __invoke(Request $request, string $userName): View
     {
-        return $userName . 'さんがフォロー中';
+        $user = User::where('user_name', $userName)->firstOrFail();
+        $relations = Follows::where('following_user_id', $user->id)->get();
+        $users = array();
+        foreach ($relations as $relation) {
+            $user = User::where('id', $relation->followed_user_id)->first();
+            array_push($users,$user);
+        }
+
+        return view('user.follows')->with([
+            'displayName' => $user->display_name,
+            'users' => $users,
+        ]);
     }
 }
