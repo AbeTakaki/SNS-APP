0cf770e80ea810f803c3d8f95e14c71775a67bcb
AbeTakaki <abetakaki0101@gmail.com>
2025-12-02 22:12:59 +0900

    feat: 各画面のUIを実装

--- src/resources/views/user/index.blade.php
+++ src/resources/views/user/index.blade.php
@@ -1,53 +1,55 @@
-<!doctype html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Xweet</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-  <h1>Xweet</h1>
-  <h2>{{ $displayName }}さんのページ</h2>
-  <p>{{$profile}}</p>
-  <button onClick="location.href='/user/{{$userName}}/follows'">フォローリストへ</button>
-  <button onClick="location.href='/user/{{$userName}}/followers'">フォロワーリストへ</button>
-  @if (\Illuminate\Support\Facades\Auth::id() !== $id)
-    @if(!$isFollowing)
-      {{-- フォロー --}}
-      <div>
-        <form action="/user/{{$userName}}/follow" method="post">
-          @csrf
-            <button type="submit">フォローする</button>
-        </form>
-      </div>
-    @else
-      {{-- アンフォロー --}}
-      <div>
-        <form action="/user/{{$userName}}/unfollow" method="post">
-          @method('DELETE')
-          @csrf
-          <button type="submit">フォロー解除</button>
-        </form>
-      </div>
-    @endif
-      {{-- チャット開始 --}}
-      <div>
-          <form action="/user/{{$userName}}/chat" method="post">
-              @csrf
-              <button type="submit">チャットを開始</button>
-          </form>
-      </div>
-  @else
-    <div>
-      <button onClick="location.href='/user/{{$userName}}/edit'">プロフィールを編集</button>
-    </div>
-  @endif
+<x-layout>
 
-  @foreach ($xweets as $xweet)
-    <p>{{ $xweet->content }} by {{ $xweet->getDisplayName() }} posted on {{ $xweet->created_at }}</p>
-  @endforeach
-</body>
-</html>
+    <x-header></x-header>
+    <x-main>
+        <div class="h-8"></div>
+        <div class="flex justify-between">
+            <div class="ml-8">
+                <h2 class="text-3xl font-bold mb-4">{{$displayName}}</h2>
+                <p>{!!nl2br(e($profile))!!}</p>
+            </div>
+
+            <div>
+                <ul class="flex space-x-4">
+                    <li><a href="{{route('user.follows',['userName'=>$userName])}}" class="text-center text-gray-500 hover:text-black">Follows</a></li>
+                    <li><a href="{{route('user.followers',['userName'=>$userName])}}" class="text-center text-gray-500 hover:text-black">Followers</a></li>
+                </ul>
+            </div>
+        </div>
+
+        @auth
+            <div class="flex flex-wrap justify-center">
+                @if(\Illuminate\Support\Facades\Auth::id()!==$id)
+                    @if(!$isFollowing)
+                        <div>
+                            <form action="/user/{{$userName}}/follow" method="post">
+                                @csrf
+                                <x-element.button-post>フォローする</x-element.button-post>
+                            </form>
+                        </div>
+                    @else
+                        <div>
+                            <form action="/user/{{$userName}}/unfollow" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <x-element.button-post>フォロー解除</x-element.button-post>
+                            </form>
+                        </div>
+                    @endif
+                    <div>
+                        <form action="/user/{{$userName}}/chat" method="post">
+                            @csrf
+                            <x-element.button-post>チャットを開始</x-element.button-post>
+                        </form>
+                    </div>
+                @else
+                    <x-element.link-get :href="route('user.edit',['userName'=>$userName])">プロフィールを編集</x-element.link-get>
+                @endif
+            </div>
+        @endauth
+
+        <x-xweet.list :xweets="$xweets"></x-xweet.list>
+
+    </x-main>    
+    
+</x-layout>
